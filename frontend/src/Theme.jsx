c51253c20e994002221c073acf24c96a79cb81c5
Mason Turner <turner_m@lynchburg.edu>
2023-04-27 10:50:19 -0400

    Configure MUI

---
+++ frontend/src/Theme.jsx
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material'
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#3E00FF',
+            contrastText: '#F5ECEF'
+        },
+    },
+    typography: {
+        fontFamily: "Inter, sans-serif"
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: `@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap')`
+        }
+    }
+
+});
+
+export default theme
