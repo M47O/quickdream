860c5f822d817ee5858aadd82b7163024194197c
Mason Turner <turner_m@lynchburg.edu>
2023-05-03 08:37:14 -0400

    Add myRoutes route

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const postsController = require("../controllers/posts");
 const authMiddleware = require("../middleware/authMiddleware")
 
-router.get("/:id", postsController.getPost)
+router.get("/myPosts", authMiddleware, postsController.getMyPosts)
+
 router.post("/create", authMiddleware, postsController.createPost)
 // router.put("post/like", postsController.likePost)
-// router.delete("/post", postsController.deletePost)
-
+router.delete("/delete", authMiddleware, postsController.deletePost)
 module.exports = router;
