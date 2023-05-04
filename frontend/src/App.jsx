eb998c147efe5121503a5b40540b65d04c175811
Mason Turner <turner_m@lynchburg.edu>
2023-05-04 14:50:29 -0400

    Rename user to loggedInUser

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,21 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import AuthPage from './pages/AuthPage'
 import ProfilePage from './pages/ProfilePage'
+import ProfileByIdPage from './pages/ProfileByIdPage'
 import Header from './components/Header'
 
 import { ThemeProvider } from '@mui/material/styles'
 import theme from './Theme'
 
 function App() {
-  let [user, setUser] = useState(null)
+  let [loggedInUser, setLoggedInUser] = useState(null)
 
   const getUser = useEffect(() => {
     async function fetchData() {
       try {
         const response = await fetch("http://localhost:4000/auth/user", { credentials: "include" });
         const data = await response.json();
-        setUser({ username: data.username, id: data._id, avatar: data.avatar });
+        setLoggedInUser({ username: data.username, id: data._id, avatar: data.avatar });
       } catch (error) {
         console.error(error);
       }
@@ -28,22 +29,23 @@ function App() {
   // console.log(user)
 
   const handleLogin = (user) => {
-    setUser(user)
+    setLoggedInUser(user)
   }
 
   const handleLogout = () => {
-    setUser(null)
+    setLoggedInUser(null)
   }
 
   return (
     <ThemeProvider theme={theme}>
-      <Header user={user} handleLogout={handleLogout} />
+      <Header loggedInUser={loggedInUser} handleLogout={handleLogout} />
       <div className="container">
         <main>
           <Routes>
-            <Route path="/" element={<HomePage user={user} />} />
-            <Route path="/auth" element={<AuthPage user={user} handleLogin={handleLogin} />} />
-            <Route path="/profile" element={<ProfilePage user={user} />} />
+            <Route path="/" element={<HomePage loggedInUser={loggedInUser} />} />
+            <Route path="/auth" element={<AuthPage loggedInUser={loggedInUser} handleLogin={handleLogin} />} />
+            <Route path="/profile" element={<ProfilePage loggedInUser={loggedInUser} />} />
+            <Route path="/profile/:id" element={<ProfileByIdPage loggedInUser={loggedInUser} />} />
           </Routes>
         </main>
       </div>
