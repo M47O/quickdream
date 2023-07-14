c0ea27b190b8ea0c97fc2098ec9672c1a9390edf
Mason Turner <turner_m@lynchburg.edu>
2023-07-14 14:07:52 -0400

    Remove comment

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,5 +1,4 @@
 const Post = require("../models/Post");
-// const Comment = require("../models/Comment");
 const User = require("../models/User")
 const openai = require("../config/openai")
 const cloudinary = require("../config/cloudinary")
